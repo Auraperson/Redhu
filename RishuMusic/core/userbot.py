d210660a659b8ebdc9591981c22b18b9a1d5ffbf
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-08-08 06:31:57 +0530

    Update userbot.py

--- RishuMusic/core/userbot.py
+++ RishuMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("UseSense")
-                await self.one.join_chat("AuraVisual")
+                await self.one.join_chat("Redhu321")
+                await self.one.join_chat("hert_beat_fm_update")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("UseSense")
-                await self.one.join_chat("AuraVisual")
+                await self.two.join_chat("Redhu321")
+                await self.one.join_chat("hert_beat_fm_update")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("UseSense")
-                await self.one.join_chat("AuraVisual")
+                await self.three.join_chat("Redhu321")
+                await self.one.join_chat("hert_beat_fm_update")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("UseSense")
-                await self.one.join_chat("AuraVisual")
+                await self.four.join_chat("Redhu321")
+                await self.one.join_chat("hert_beat_fm_update")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("UseSense")
-                await self.one.join_chat("AuraVisual")
+                await self.five.join_chat("Redhu321")
+                await self.one.join_chat("hert_beat_fm_update")
             except:
                 pass
             assistants.append(5)
